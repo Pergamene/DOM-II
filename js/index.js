899734556a76f47808e42a3467b883e96a17678d
Pergamene <smartz373@gmail.com>
2020-02-11 16:16:02 -0700

    finished preventDefault

--- js/index.js
+++ js/index.js
@@ -4,18 +4,20 @@ const imgContent = document.querySelectorAll('.img-content');
 const boatImg = document.querySelector('.content-destination img');
 
 // resize
-window.addEventListener('resize', () => {
+window.addEventListener('resize', event => {
   textItems.forEach(textItem => {
     textItem.style['letter-spacing'] = '-.2em';
   });
+  event.preventDefault();
 });
 
 // scroll
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', event => {
   textItems.forEach(textItem => {
     textItem.style['letter-spacing'] = 'normal';
     textItem.style.color = 'black';
   });
+  event.preventDefault();
 });
 
 // keydown
@@ -25,6 +27,7 @@ window.addEventListener('keydown', event => {
       textItem.style.transform = 'translate(0, -20px)';
     });
   }
+  event.preventDefault();
 });
 
 // keyup
@@ -34,47 +37,54 @@ window.addEventListener('keyup', event => {
       textItem.style.transform = 'translate(0, 0)';
     });
   }
+  event.preventDefault();
 });
 
 // click
-busImg.addEventListener('click', () => {
+busImg.addEventListener('click', event => {
   gsap.to(busImg, {
     duration: 1,
     rotateY: 360
   });
+  event.preventDefault();
 });
 
 // dblclick
 imgContent.forEach(img => {
-  img.addEventListener('dblclick', () => {
+  img.addEventListener('dblclick', event => {
     img.style.display = 'none';
   });
+  event.preventDefault();
 });
 
 // mousedown
-boatImg.addEventListener('mousedown', () => {
+boatImg.addEventListener('mousedown', event => {
   imgContent.forEach(img => {
     img.style.display = 'block';
   });
+  event.preventDefault();
 });
 
 // mouseenter
-busImg.addEventListener('mouseenter', () => {
+busImg.addEventListener('mouseenter', event => {
   textItems.forEach(textItem => {
     textItem.style['font-style'] = 'italic';
   });
+  event.preventDefault();
 });
 
 // mouseleave
-busImg.addEventListener('mouseleave', () => {
+busImg.addEventListener('mouseleave', event => {
   textItems.forEach(textItem => {
     textItem.style['font-style'] = 'normal';
   });
+  event.preventDefault();
 });
 
 // mousemove
-window.addEventListener('mousemove', () => {
+window.addEventListener('mousemove', event => {
   textItems.forEach(textItem => {
     textItem.style.color = 'red';
   });
+  event.preventDefault();
 });
