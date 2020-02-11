168d44a012fa135ea4cfa43662fa65cc3454a5b7
Pergamene <smartz373@gmail.com>
2020-02-11 15:00:00 -0700

    four events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
-// Your code goes here
+const textItems = document.querySelectorAll('h1, h2, h4, a, p');
+
+// resize
+window.addEventListener('resize', () => {
+  textItems.forEach(textItem => {
+    textItem.style['letter-spacing'] = '-.2em';
+  });
+});
+
+// scroll
+window.addEventListener('scroll', () => {
+  textItems.forEach(textItem => {
+    textItem.style['letter-spacing'] = 'normal';
+  });
+});
+
+// keydown
+window.addEventListener('keydown', event => {
+  if (event.keyCode === 74) {
+    textItems.forEach(textItem => {
+      textItem.style.transform = 'translate(0, -20px)';
+    });
+  }
+});
+
+// keyup
+window.addEventListener('keyup', event => {
+  if (event.keyCode === 74) {
+    textItems.forEach(textItem => {
+      textItem.style.transform = 'translate(0, 0)';
+    });
+  }
+});
+
+// click
+
+// dblclick
+
+// mousedown
+
+// mouseenter
+
+// mousemove
+
+// select
